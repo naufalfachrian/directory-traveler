3cecc05a97ff5bdc5f17d6f5f7413a0fccbc32cf
Naufal Fachrian <naufal.fachrian@gmail.com>
2018-01-11 18:21:06 +0700

    press back button twice to exit

--- app/src/main/java/id/sugarknife/hibikihagyu/activity/DirectoryActivity.kt
+++ app/src/main/java/id/sugarknife/hibikihagyu/activity/DirectoryActivity.kt
@@ -3,7 +3,9 @@ package id.sugarknife.hibikihagyu.activity
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.os.Environment
+import android.os.Handler
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
 import id.sugarknife.hibikihagyu.R
 import id.sugarknife.hibikihagyu.extension.askStoragePermission
 import id.sugarknife.hibikihagyu.extension.hasStoragePermission
@@ -15,6 +17,8 @@ class DirectoryActivity : AppCompatActivity() {
     private val askStoragePermissionRequestCode = 431
 
     private val fragmentTag = "DirectoryFragment"
+
+    private var shouldExit = false
 
     var currentDirectory: File? = null
         set(value) {
@@ -58,9 +62,21 @@ class DirectoryActivity : AppCompatActivity() {
 
     override fun onBackPressed() {
         if (currentDirectory == DirectoryActivity.rootDirectory) {
-            super.onBackPressed()
+            pressBackButtonTwiceToExit()
         } else {
             currentDirectory = currentDirectory?.parentFile
+        }
+    }
+
+    private fun pressBackButtonTwiceToExit() {
+        if (shouldExit) {
+            super.onBackPressed()
+        } else {
+            Toast.makeText(this, getString(R.string.exit_toast), Toast.LENGTH_SHORT).show()
+            shouldExit = true
+            Handler().postDelayed({
+                shouldExit = false
+            }, 1000)
         }
     }
 
